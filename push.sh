a96a3bfa9c4f6e9c3422ae5ed419454b916eaee6
Tyler Uebele <tyler.uebele@gmail.com>
2018-08-12 13:56:25 -0400

    Add init.php to initialize database

--- push.sh
+++ push.sh
@@ -3,4 +3,5 @@
 rsync -rltv --exclude=.git --delete-excluded ../Graphite/src/ ./vendor/stationer/graphite/src/
 rsync -rltv --exclude=.git --delete-excluded ../Barrel/src/ ./vendor/stationer/barrel/src/
 rsync -rltv --exclude=.git --delete-excluded ../Pencil/src/ ./vendor/stationer/pencil/src/
-rsync -rltvR --exclude=.git --delete src/ vendor/ lonefry@lonefry.com:dev.lonefry.com/
+rsync -rltvR --exclude=.git --delete src/init.php vendor/ lonefry@lonefry.com:dev.lonefry.com/
+rsync -rltv --exclude=.git --exclude=vendor/ --delete src/ lonefry@lonefry.com:dev.lonefry.com/
