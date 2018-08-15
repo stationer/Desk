08e3fe249bc6dc5419b67d3c21bca47b35c7ed25
Tyler Uebele <tyler.uebele@gmail.com>
2018-08-14 20:31:40 -0400

    Adjust push.sh, add robots.txt

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rsync -rltv --exclude=.git --delete-excluded ../Graphite/src/ ./vendor/stationer/graphite/src/
-rsync -rltv --exclude=.git --delete-excluded ../Barrel/src/ ./vendor/stationer/barrel/src/
-rsync -rltv --exclude=.git --delete-excluded ../Pencil/src/ ./vendor/stationer/pencil/src/
+rsync -rltv --exclude=.git --delete-excluded ../Graphite/ ./vendor/stationer/graphite/
+rsync -rltv --exclude=.git --delete-excluded ../Barrel/ ./vendor/stationer/barrel/
+rsync -rltv --exclude=.git --delete-excluded ../Pencil/ ./vendor/stationer/pencil/
 rsync -rltvR --exclude=.git --delete src/init.php vendor/ lonefry@lonefry.com:dev.lonefry.com/
 rsync -rltv --exclude=.git --exclude=vendor/ --delete src/ lonefry@lonefry.com:dev.lonefry.com/
