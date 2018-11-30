94a69429b089a975e938496501ed0cad45375b58
Adam Stafford <adam.stafford@bettercarpeople.com>
2018-11-30 16:49:43 -0500

    login page stuff

--- push.sh
+++ push.sh
@@ -4,5 +4,5 @@ rsync -rltv --exclude=.git/ --delete-excluded ../Graphite/ ./vendor/stationer/gr
 rsync -rltv --exclude=.git/ --delete-excluded ../Barrel/ ./vendor/stationer/barrel/
 rsync -rltv --exclude=.git/ --delete-excluded ../Pencil/ ./vendor/stationer/pencil/
 rsync -rltv --exclude=.git/ --delete-excluded ../LastWord/ ./vendor/stationer/lastword/
-rsync -rltvR --exclude=.git/ --delete src/init.php vendor/ lonefry@lonefry.com:dev.lonefry.com/
-rsync -rltv --exclude=.git/ --exclude=vendor/ --delete src/ lonefry@lonefry.com:dev.lonefry.com/
+rsync -rltvR --exclude=.git/ --delete src/init.php vendor/ adamstafford@adam.lonefry.com:adam.lonefry.com/
+rsync -rltv --exclude=.git/ --exclude=vendor/ --delete src/ adamstafford@adam.lonefry.com:adam.lonefry.com/
