e45aac459a4d6c3193e48bcf3a6b44e94d035802
Tyler Uebele <tyler.uebele@gmail.com>
2018-12-02 19:07:19 -0500

    Update push.sh
    Accept destination as a paramter
    Grab letterhead styles into Pencil before upload

--- push.sh
+++ push.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# A script for pushing changes to an already deployed site
+
+# Grab latest Graphite code from sister repo
 rsync -rltv --exclude=.git/ --delete-excluded ../Graphite/ ./vendor/stationer/graphite/
+
+# Grab latest Barrel code from sister repo
 rsync -rltv --exclude=.git/ --delete-excluded ../Barrel/ ./vendor/stationer/barrel/
+
+# Grab latest Pencil code from sister repo, and associated assets from Letterhead and SortTable
+rsync -rltv --exclude=.git/ --delete-excluded ../Letterhead/public/ ../Pencil/node_modules/letterhead/public/
+rsync -rltv --exclude=.git/ --delete-excluded ../Letterhead/public/css/app.css ../Pencil/src/css/letterhead.css
+rsync -rltv --exclude=.git/ --delete-excluded ../Letterhead/public/js/app.js ../Pencil/src/js/letterhead.js
+rsync -rltv --exclude=.git/ --delete-excluded ../SortTable/sort-table.min.js ../Pencil/src/js/oksort.js
 rsync -rltv --exclude=.git/ --delete-excluded ../Pencil/ ./vendor/stationer/pencil/
+
+# Grab latest LastWord code from sister repo, and associated assets from Letterhead
+rsync -rltv --exclude=.git/ --delete-excluded ../Letterhead/public/js/app.js ../LastWord/src/js/letterhead.js
+rsync -rltv --exclude=.git/ --delete-excluded ../Letterhead/public/css/app.css ../LastWord/src/css/letterhead.css
 rsync -rltv --exclude=.git/ --delete-excluded ../LastWord/ ./vendor/stationer/lastword/
-rsync -rltvR --exclude=.git/ --delete src/init.php vendor/ lonefry@lonefry.com:dev.lonefry.com/
-rsync -rltv --exclude=.git/ --exclude=vendor/ --delete src/ lonefry@lonefry.com:dev.lonefry.com/
+
+# rsync latest code up to specified host
+rsync -rltvR --exclude=.git/ --delete src/init.php vendor/ $1
+rsync -rltv --exclude=.git/ --exclude=p.uploads/ --exclude=P_Cache/ --exclude=vendor/ --delete src/ $1
